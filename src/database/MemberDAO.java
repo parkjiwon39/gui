33473d8e5108b9144bf8152acbcd6ab63166f6ea
parkjiwon <parkjw9585@gmail.com>
2020-04-08 15:43:50 +0900

    멤버DAO

--- src/database/MemberDAO.java
+++ src/database/MemberDAO.java
@@ -24,7 +24,7 @@ public class MemberDAO {
 		
 		Connection con=null;
 		try {
-			DriverManager.getConnection(url, user, password);
+			con=DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
